79b009ab56a7ec71a2efaf7a96b5709577d555a1
2496xin <2496xin@gmail.com>
2018-04-26 20:53:26 -0400

    package projectview

---
+++ Animator.java
@@ -0,0 +1,35 @@
+package projectview;
+import javax.swing.Timer;
+
+public class Animator {
+	private static final int TICK = 500;
+	private boolean autoStepOn = false;
+	private Timer timer;
+	private ViewMediator view;
+	
+	public Animator(ViewMediator view) {
+		this.view = view;
+	}
+
+	public boolean isAutoStepOn() {
+		return autoStepOn;
+	}
+
+	public void setAutoStepOn(boolean autoStepOn) {
+		this.autoStepOn = autoStepOn;
+	}
+	
+	void toggleAutoStep() {
+		if (autoStepOn = true) autoStepOn = false;
+		else autoStepOn = true;
+	}
+	
+	void setPeriod(int period) {
+		timer.setDelay(period);
+	}
+	
+	public void start() {
+		timer = new Timer(TICK, e -> {if(autoStepOn) view.step();});
+		timer.start();
+	}
+}
