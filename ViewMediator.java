23c0aeeeb6e837a82bd169a810fea3c043d17f3b
akraus5 <akraus5@binghamton.edu>
2018-04-28 14:34:31 -0400

    Add files via upload

--- ViewMediator.java
+++ ViewMediator.java
@@ -3,6 +3,7 @@ package projectview;
 import java.util.Observable;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import project.MachineModel;
 import java.awt.Container;
@@ -60,32 +61,25 @@ public class ViewMediator extends Observable {
 		frame = new JFrame("Simulator");//syntax
 		Container content = frame.getContentPane();
 		
-		//TODO
-		/*Set the layout of content using (FYI a tutorial on layouts is at http://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html)
-
 		content.setLayout(new BorderLayout(1,1));
 		content.setBackground(Color.BLACK);
-		This will separate all the components in the GUI with a 1 pixel black separation.
 		
-		Use setSize to set the frame size to 1200x600
+		frame.setSize(1200,600);//??
+		JPanel center = new GridLayout(1, 3);
+		frame.add(codeViewPanel.createCodeDisplay(),BorderLayout.LINE_START);
+		center.add(memoryViewPanel1.createMemoryDisplay());
+		center.add(memoryViewPanel2.createMemoryDisplay());
+		center.add(memoryViewPanel3.createMemoryDisplay());
 		
-		Make a JPanel center and set its layout to new GridLayout(1, 3)
-		
-		Add codeViewPanel.createCodeDisplay() to frame at position BorderLayout.LINE_START
-		
-		That means frame.add(codeViewPanel.createCodeDisplay(),BorderLayout.LINE_START);
-		
-		Add memoryViewPanel1.createMemoryDisplay(), memoryViewPanel2.createMemoryDisplay(), memoryViewPanel3.createMemoryDisplay() to the JPanel center. A grid layout (1,3) will put the three components side by side in equal amounts of space.
+		//A grid layout (1,3) will put the three components side by side in equal amounts of space.
 			
-		Add center to frame at position BorderLayout.CENTER (this is the default location in a BorderLayout so the add alone is enough)
+		frame.add(center,BorderLayout.CENTER); //"return HERE for the other GUI components."
 	
-		Put in a comment that we have to "return HERE for the other GUI components."
-	
-		Add the lines about closing the GUI (although we will replace it later) and showing the GUI
+		//Add the lines about closing the GUI (although we will replace it later) and showing the GUI
 	
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// return HERE for other setup details
-		frame.setVisible(true);*/
+		frame.setVisible(true);
  
 	}
 	
@@ -102,13 +96,26 @@ public class ViewMediator extends Observable {
 			}
 		});
 	}
-	/*Write delegate methods States getCurrentState() and void setCurrentState(States currentState) that are delegated to model. However the set method has additional code. At the state of this set method if currentState == States.PROGRAM_HALTED, then call animator.setAutoStepOn(false); (Recall that the contants of an enum are Singleton objects--there can only be one copy in the whole program. Therefore we can safely use == for equality). After calling the setCurrentState in model, we must repeat the three lines that always follow a change of state (let's call these the 3 NOTIFY LINES):
+	States getCurrentState() {
+		return //??
+	}
+	void setCurrentState(States currentState){
+		return //??
+	}
+	
+	/*
+	At the state of this set method if currentState == States.PROGRAM_HALTED, then call animator.setAutoStepOn(false); 
+	(Recall that the contants of an enum are Singleton objects--there can only be one copy in the whole program. 
+	Therefore we can safely use == for equality). After calling the setCurrentState in model, we must repeat the 
+	three lines that always follow a change of state (let's call these the 3 NOTIFY LINES):
 
 	model.getCurrentState().enter();
+	
 	setChanged();
 	notifyObservers();
 	The call to the enter of a new state just makes sure the boolean array in the object is correct and then the notification mechanism of the Observer Pattern is invoked.
 	 */
+	
 	public void exit() { // method executed when user exits the program
 		int decision = JOptionPane.showConfirmDialog(
 				frame, "Do you really wish to exit?",
@@ -121,5 +128,5 @@ public class ViewMediator extends Observable {
 	public JFrame getFrame() {
 		return frame;
 	}
-	}
+	
 }
